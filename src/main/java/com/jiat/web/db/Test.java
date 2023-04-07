a8c555aed49829809bb1eeaf201167dc0d7e4c61
Rush Perera <104535750+Rush-Perera@users.noreply.github.com>
2023-04-07 19:52:32 +0530

    WEB2 lecture 03-04-23 parts
    WEB2 lecture 03-04-23 updates are done to the same project repo

--- src/main/java/com/jiat/web/db/Test.java
+++ src/main/java/com/jiat/web/db/Test.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 @WebServlet(name = "test", urlPatterns = "/test")
 public class Test extends HttpServlet {
@@ -15,16 +17,24 @@ public class Test extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Connection connection = null;
-        try{
+        try {
+
             connection = DBConnection.getConnection();
-            System.out.println(connection);
-        }catch (Exception e){
+
+            PreparedStatement preparedStatement = connection.prepareStatement("");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("username"));
+            }
+
+
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
-            if (connection != null){
-                try{
+        } finally {
+            if (connection != null) {
+                try {
                     connection.close();
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
